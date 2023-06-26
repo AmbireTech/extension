825aff3120a01fe95aeadec08c711d5302604c05
Simeon Krasimirov Petkov <simeonpetkov11999977@gmail.com>
2023-06-26 18:28:08 +0300

    login screens logo removed and segments replaced with clickable text

--- src/mobile/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
+++ src/mobile/modules/auth/screens/JsonLoginScreen/JsonLoginScreen.tsx
@@ -7,9 +7,9 @@ import Text from '@common/components/Text'
 import Wrapper from '@common/components/Wrapper'
 import { isWeb } from '@common/config/env'
 import { useTranslation } from '@common/config/localization'
-import AmbireLogo from '@common/modules/auth/components/AmbireLogo'
 import useJsonLogin from '@common/modules/auth/hooks/useJsonLogin'
 import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 
 const JsonLoginScreen = () => {
   const { t } = useTranslation()
@@ -18,20 +18,21 @@ const JsonLoginScreen = () => {
   return (
     <GradientBackgroundWrapper>
       <Wrapper contentContainerStyle={spacings.pbLg}>
-        <AmbireLogo />
-        <Button
-          disabled={inProgress}
-          text={inProgress ? t('Importing...') : t('Select File')}
-          onPress={() => handleLogin({})}
-          hasBottomSpacing={!error || isWeb}
-        />
-        {!!error && (
-          <View style={spacings.ptTy}>
-            <Text appearance="danger" fontSize={12} style={spacings.ph}>
-              {error}
-            </Text>
-          </View>
-        )}
+        <View style={[flexbox.flex1, flexbox.justifyEnd]}>
+          <Button
+            disabled={inProgress}
+            text={inProgress ? t('Importing...') : t('Select File')}
+            onPress={() => handleLogin({})}
+            hasBottomSpacing={!error || isWeb}
+          />
+          {!!error && (
+            <View style={spacings.ptTy}>
+              <Text appearance="danger" fontSize={12} style={spacings.ph}>
+                {error}
+              </Text>
+            </View>
+          )}
+        </View>
       </Wrapper>
     </GradientBackgroundWrapper>
   )
