d12fbf5b31e3ce0e9f476ff77ecfdb6baed45021
superKalo <superkalo@devlabs.bg>
2023-07-10 18:45:39 +0300

    Add: the initial structure of the normalize method + types for v1 and v2

---
+++ src/common/contexts/portfolioContext/normalize.ts
@@ -0,0 +1,226 @@
+interface AssetV2 {
+  tokenId: string
+  balance: string
+  original_owner: string
+  token_url: string
+  data: {
+    name: string
+    description: string
+    image: string
+    image_256: string
+    image_512: string
+    image_1024: string
+    attributes: {
+      trait_type: string
+      value: string
+    }[]
+  }
+}
+
+interface NFTV2 {
+  type: string
+  address: string
+  decimals: number
+  symbol: string
+  price: number
+  balance: string
+  balanceUSD: number
+  shouldDisplay: boolean
+  collectionId: string
+  collectionName: string
+  collectionHidden: boolean
+  collection: {
+    id: string
+    name: string
+    hidden: boolean
+  }
+  assets: AssetV2[]
+}
+
+interface TokenV2 {
+  type: string
+  address: string
+  decimals: number
+  symbol: string
+  name: string
+  coingeckoId: string
+  tokenImageUrl: string
+  tokenImageUrls: {
+    thumb: string
+    small: string
+    large: string
+  }
+  balance: number
+  balanceRaw: string
+  price: number
+  balanceUSD: number
+  priceUpdate: number
+  balanceUpdate: number
+}
+
+interface ResponseV2 {
+  success: boolean
+  data: {
+    success: boolean
+    tokens: TokenV2[]
+    nfts: NFTV2[]
+    resultTime: number
+    provider: string
+    error: null | string
+    identity: string
+    network: string
+  }
+}
+
+interface TokenV1 {
+  type: string
+  network: string
+  address: string
+  decimals: number
+  symbol: string
+  price: number
+  balance: number
+  balanceRaw: string
+  balanceUSD: number
+  tokenImageUrl: string
+}
+
+interface NFTV1 {
+  type: string
+  network: string
+  address: string
+  decimals: number
+  symbol: string
+  price: number
+  balance: string
+  balanceRaw: string
+  balanceUSD: number
+  tokenImageUrl: string
+  category: string
+  shouldDisplay: boolean
+  collectionId: string
+  collectionName: string
+  collectionHidden: boolean
+  collectionImg: string
+  collection: {
+    id: string
+    name: string
+    hidden: boolean
+    floorPrice: number
+    floorPriceUSD: number
+    owners: number
+    items: number
+    volume24h: number
+    volume24hUSD: number
+  }
+  assets: {
+    tokenId: string
+    balance: string
+    assetImg: string
+    balanceUSD: number
+  }[]
+}
+
+interface ResponseV1 {
+  [address: string]: {
+    products: {
+      label: string
+      assets: (TokenV1 | NFTV1)[]
+    }[]
+    meta: {
+      label: string
+      value: number
+      type: string
+    }[]
+    systemInfo: {
+      source: number
+      updateAt: number
+      nextUpdate: number
+    }
+  }
+}
+
+export function normalizeResponse(response: ResponseV2): ResponseV1 {
+  const v1Response: ResponseV1 = {
+    [response.data.identity]: {
+      products: [],
+      meta: [],
+      systemInfo: {
+        source: 5, // TODO: static source value?
+        updateAt: response.data.resultTime,
+        nextUpdate: response.data.resultTime + 600000 // TODO: Assume update interval of 10 minutes?
+      }
+    }
+  }
+
+  // Normalizing tokens
+  if (response.data.tokens) {
+    const tokenAssets: TokenV1[] = response.data.tokens.map((token) => {
+      return {
+        type: 'wallet',
+        network: response.data.network,
+        address: token.address,
+        decimals: token.decimals,
+        symbol: token.symbol,
+        price: token.price,
+        balance: token.balance,
+        balanceRaw: token.balanceRaw,
+        balanceUSD: token.balanceUSD,
+        tokenImageUrl: token.tokenImageUrl
+      }
+    })
+    v1Response[response.data.identity].products.push({
+      label: 'Tokens',
+      assets: tokenAssets
+    })
+  }
+
+  // Normalizing NFTs
+  if (response.data.nfts) {
+    const nftAssets: NFTV1[] = response.data.nfts.map((nft) => {
+      return {
+        type: 'nft',
+        network: response.data.network,
+        address: nft.address,
+        decimals: nft.decimals,
+        symbol: nft.symbol,
+        price: nft.price,
+        balance: nft.balance,
+        balanceRaw: nft.balance, // As NFTs usually don't have decimal places, raw balance is same as balance.
+        balanceUSD: nft.balanceUSD,
+        tokenImageUrl: nft.assets[0]?.token_url || '', // Assuming there's at least one asset.
+        category: 'NFT',
+        shouldDisplay: nft.shouldDisplay,
+        collectionId: nft.collectionId,
+        collectionName: nft.collectionName,
+        collectionHidden: nft.collectionHidden,
+        collectionImg: nft.assets[0]?.token_url || '', // Assuming there's at least one asset.
+        collection: {
+          id: nft.collection.id,
+          name: nft.collection.name,
+          hidden: nft.collection.hidden,
+          floorPrice: 0, // Placeholder, as we don't have floor price in the v2 response
+          floorPriceUSD: 0, // Placeholder, as we don't have floor price in USD in the v2 response
+          owners: 0, // Placeholder, as we don't have owner information in the v2 response
+          items: nft.assets.length,
+          volume24h: 0, // Placeholder, as we don't have 24h volume in the v2 response
+          volume24hUSD: 0 // Placeholder, as we don't have 24h volume in USD in the v2 response
+        },
+        assets: nft.assets.map((asset) => {
+          return {
+            tokenId: asset.tokenId,
+            balance: asset.balance,
+            assetImg: asset.token_url,
+            balanceUSD: 0 // Placeholder, as we don't have asset-specific balance in USD in the v2 response
+          }
+        })
+      }
+    })
+    v1Response[response.data.identity].products.push({
+      label: 'Collectibles',
+      assets: nftAssets
+    })
+  }
+
+  return v1Response
+}
