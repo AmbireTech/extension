6635b19ec380df082128b30bcda1073b5d637505
superKalo <superkalo@devlabs.bg>
2023-07-19 13:09:35 +0300

    Add: switch back to the found token item UI

--- src/common/modules/dashboard/components/AddOrHideToken/AddOrHideTokenForm.tsx
+++ src/common/modules/dashboard/components/AddOrHideToken/AddOrHideTokenForm.tsx
@@ -15,7 +15,7 @@ import useToken from '@common/hooks/useToken'
 import spacings from '@common/styles/spacings'
 
 import { MODES } from './constants'
-import TokenItem from './HideTokenListItem'
+import FoundTokenItem from './FoundTokenItem'
 
 const ADDRESS_LENGTH = 42
 const TOKEN_SYMBOL_MIN_LENGTH = 3
@@ -169,8 +169,7 @@ const AddOrHideTokenForm: React.FC<Props> = ({
         </View>
       )}
 
-      {/* TODO: Should be different */}
-      {!showError && tokenDetails && <TokenItem {...tokenDetails} />}
+      {!showError && tokenDetails && <FoundTokenItem {...tokenDetails} />}
 
       <Button
         text={isSubmitting ? buttonSubmittingText : buttonSubmitText}
