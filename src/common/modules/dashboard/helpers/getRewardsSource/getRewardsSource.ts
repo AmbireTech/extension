d043d7614a9cf1fe165f8e026f5fc4d770793148
Gergana Mihaylova <gergana@devlabs.bg>
2023-10-20 15:06:55 +0300

    Add: rewards types from common

--- src/common/modules/dashboard/helpers/getRewardsSource/getRewardsSource.ts
+++ src/common/modules/dashboard/helpers/getRewardsSource/getRewardsSource.ts
@@ -1,15 +1,7 @@
+import { RewardsSource } from 'ambire-common/src/hooks/useRewards/types'
+
 import { isAndroid, isiOS, isWeb } from '@common/config/env'
 import { isExtension } from '@web/constants/browserapi'
-
-// FIXME: add this enum in ambire-common v0.22 series (it exists in v0.21 only)
-// import { RewardsSource } from 'ambire-common/src/hooks/useRewards/types'
-export enum RewardsSource {
-  UNSET = 'unset',
-  WEB = 'web',
-  ANDROID = 'android',
-  IOS = 'ios',
-  EXTENSION = 'extension'
-}
 
 const getRewardsSource = () => {
   if (isAndroid) return RewardsSource.ANDROID
