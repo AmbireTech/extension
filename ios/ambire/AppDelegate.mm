ffb4238ad669cb3d7eae944e121f913a0b3ecc0a
Gergana Mihaylova <gergana@devlabs.bg>
2024-01-22 19:05:18 +0200

    Add: production build fixes

--- ios/ambire/AppDelegate.mm
+++ ios/ambire/AppDelegate.mm
@@ -1,12 +1,10 @@
 #import "AppDelegate.h"
 #import <React/RCTAppSetupUtils.h>
-
+#import <EXSplashScreen/EXSplashScreenService.h>
 
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
-
-#import <React/RCTAppSetupUtils.h>
 
 @interface AppDelegate ()
 
@@ -18,57 +16,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"ambire";
+  self.moduleName = @"main";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
-  
-  self.launchOptions = launchOptions;
-  self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-  #ifdef DEBUG
-    [self initializeReactNativeApp];
-  #else
-    EXUpdatesAppController *controller = [EXUpdatesAppController sharedInstance];
-    controller.delegate = self;
-    [controller startAndShowLaunchScreen:self.window];
-  #endif
 
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
-}
-
-- (RCTBridge *)initializeReactNativeApp
-{
-  NSDictionary *initProps = [self prepareInitialProps];
-
-  RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:self.launchOptions];
-UIView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                             moduleName:@"main"
-                                      initialProperties:initProps];
-
-  UIViewController *rootViewController = [self.reactDelegate createRootViewController];
-  rootViewController.view = rootView;
-  self.window.rootViewController = rootViewController;
-  [self.window makeKeyAndVisible];
-  return bridge;
-}
-
-/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
-///
-/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
-/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
-/// @return: `true` if the `concurrentRoot` feture is enabled. Otherwise, it returns `false`.
-- (BOOL)concurrentRootEnabled
-{
-  // Switch this bool to turn on and off the concurrent root
-  return true;
-}
-- (NSDictionary *)prepareInitialProps
-{
-  NSMutableDictionary *initProps = [NSMutableDictionary new];
-#ifdef RCT_NEW_ARCH_ENABLED
-  initProps[kRNConcurrentRoot] = @([self concurrentRootEnabled]);
-#endif
-  return initProps;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
@@ -76,12 +29,18 @@ UIView *rootView = [[RCTRootView alloc] initWithBridge:bridge
 #if DEBUG
   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index"];
 #else
-  return [[EXUpdatesAppController sharedInstance] launchAssetUrl];
+  return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
 
-// -(void)appController:(EXUpdatesAppController *)appController didStartWithSuccess:(BOOL)success {
-//   appController.bridge = [self initializeReactNativeApp];
-// }
+/// This method controls whether the `concurrentRoot`feature of React18 is turned on or off.
+///
+/// @see: https://reactjs.org/blog/2022/03/29/react-v18.html
+/// @note: This requires to be rendering on Fabric (i.e. on the New Architecture).
+/// @return: `true` if the `concurrentRoot` feature is enabled. Otherwise, it returns `false`.
+- (BOOL)concurrentRootEnabled
+{
+  return true;
+}
 
 @end
