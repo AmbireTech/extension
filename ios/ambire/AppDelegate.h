328bc8e1fcc3e2b3eb5bee4ca98e37fb0f5c4bc2
Kaloyan Kosev <superkalo@devlabs.bg>
2024-01-23 17:24:19 +0200

    Fix: Expo modules missng configuration (leading to problems with expo modules such as the splash screen one)

--- ios/ambire/AppDelegate.h
+++ ios/ambire/AppDelegate.h
@@ -1,9 +1,10 @@
 #import <Foundation/Foundation.h>
 #import <RCTAppDelegate.h>
+#import <Expo/Expo.h>
 #import <UIKit/UIKit.h>
 
 #import <Expo/Expo.h>
 
-@interface AppDelegate : RCTAppDelegate
+@interface AppDelegate : EXAppDelegateWrapper
 
 @end
