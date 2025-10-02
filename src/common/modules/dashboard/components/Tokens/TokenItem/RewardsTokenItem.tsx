48b04469a1f851ac8d7544c4994195fe1bcbd9e0
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2025-10-02 13:54:53 +0300

    Changed the button in RewardsTokenItem component

--- src/common/modules/dashboard/components/Tokens/TokenItem/RewardsTokenItem.tsx
+++ src/common/modules/dashboard/components/Tokens/TokenItem/RewardsTokenItem.tsx
@@ -1,17 +1,20 @@
 import React, { useCallback } from 'react'
+import { Pressable } from 'react-native'
 
 import { TokenResult } from '@ambire-common/libs/portfolio'
-import Button from '@common/components/Button'
+import Text from '@common/components/Text'
 import { useTranslation } from '@common/config/localization'
 import useTheme from '@common/hooks/useTheme'
 import { THEME_TYPES } from '@common/styles/themeConfig'
+import common from '@common/styles/utils/common'
+import flexbox from '@common/styles/utils/flexbox'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 
 import BaseTokenItem from './BaseTokenItem'
 
 const RewardsTokenItem = ({ token }: { token: TokenResult }) => {
   const { t } = useTranslation()
-  const { themeType } = useTheme()
+  const { themeType, theme } = useTheme()
   const { dispatch } = useBackgroundService()
 
   const sendClaimTransaction = useCallback(() => {
@@ -25,13 +28,30 @@ const RewardsTokenItem = ({ token }: { token: TokenResult }) => {
     <BaseTokenItem
       token={token}
       extraActions={
-        <Button
-          size="small"
-          type="secondary"
-          text={t('Claim')}
-          hasBottomSpacing={false}
+        <Pressable
           onPress={sendClaimTransaction}
-        />
+          style={({ hovered }: any) => [
+            flexbox.center,
+            flexbox.directionRow,
+            common.borderRadiusPrimary,
+            { width: 70, height: 38 },
+            {
+              borderWidth: 1,
+              borderColor: 'transparent',
+              backgroundColor: hovered
+                ? themeType === THEME_TYPES.DARK
+                  ? '#888C9F50'
+                  : '#F4F4F760'
+                : themeType === THEME_TYPES.DARK
+                ? '#888C9F40'
+                : '#F4F4F750'
+            }
+          ]}
+        >
+          <Text fontSize={14} weight="medium" color={theme.primary}>
+            {t('Claim')}
+          </Text>
+        </Pressable>
       }
       gradientStyle={
         themeType === THEME_TYPES.DARK
