27099e740596306b62278f7372f9b2c115b906ab
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2025-10-15 18:03:45 +0300

    Refactor code base

--- src/common/modules/dashboard/components/Tokens/TokenItem/RewardsTokenItem.tsx
+++ src/common/modules/dashboard/components/Tokens/TokenItem/RewardsTokenItem.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { Pressable } from 'react-native'
 
 import { TokenResult } from '@ambire-common/libs/portfolio'
@@ -6,22 +6,23 @@ import Text from '@common/components/Text'
 import { useTranslation } from '@common/config/localization'
 import common from '@common/styles/utils/common'
 import flexbox from '@common/styles/utils/flexbox'
-import useBackgroundService from '@web/hooks/useBackgroundService'
 
 import BaseTokenItem from './BaseTokenItem'
 
 const GRADIENT_STYLE = 'linear-gradient(90deg, #B082FF 0%, #5F02FF 100%)'
 
-const RewardsTokenItem = ({ token }: { token: TokenResult }) => {
+const RewardsTokenItem = ({
+  token,
+  onPress,
+  actionButtonText,
+  description
+}: {
+  token: TokenResult
+  onPress: () => void
+  actionButtonText: string
+  description: string
+}) => {
   const { t } = useTranslation()
-  const { dispatch } = useBackgroundService()
-
-  const sendClaimTransaction = useCallback(() => {
-    dispatch({
-      type: 'REQUESTS_CONTROLLER_BUILD_REQUEST',
-      params: { type: 'claimWalletRequest', params: { token } }
-    })
-  }, [dispatch, token])
 
   return (
     <BaseTokenItem
@@ -29,28 +30,27 @@ const RewardsTokenItem = ({ token }: { token: TokenResult }) => {
       extraActions={
         <Pressable
           testID="projected-rewards-claim-button"
-          onPress={sendClaimTransaction}
+          onPress={onPress}
           style={({ hovered }: any) => [
             flexbox.center,
             flexbox.directionRow,
             common.borderRadiusPrimary,
             {
               width: 70,
-              height: 38,
               background: GRADIENT_STYLE,
               opacity: hovered ? 0.8 : 1
             }
           ]}
         >
           <Text fontSize={14} weight="medium" color="white">
-            {t('Claim')}
+            {t('{{actionButtonText}}', { actionButtonText })}
           </Text>
         </Pressable>
       }
       gradientStyle={GRADIENT_STYLE}
       label={
         <Text fontSize={12} weight="regular">
-          {t('Claimable rewards')}
+          {t('{{description}}', { description })}
         </Text>
       }
       borderRadius={16}
--- src/common/modules/dashboard/components/Tokens/TokenItem/TokenItem.tsx
+++ src/common/modules/dashboard/components/Tokens/TokenItem/TokenItem.tsx
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 
 import { TokenResult } from '@ambire-common/libs/portfolio'
 import getAndFormatTokenDetails from '@common/modules/dashboard/helpers/getTokenDetails'
+import useBackgroundService from '@web/hooks/useBackgroundService'
 import useNetworksControllerState from '@web/hooks/useNetworksControllerState'
 import useSelectedAccountControllerState from '@web/hooks/useSelectedAccountControllerState'
+import { getUiType } from '@web/utils/uiType'
 
 import BaseTokenItem from './BaseTokenItem'
-import ProjectedRewardsTokenItem from './ProjectedRewardsTokenItem'
 import RewardsTokenItem from './RewardsTokenItem'
-import VestingTokenItem from './VestingTokenItem'
+
+const { isPopup } = getUiType()
+
+const INFO_BTN_URL = 'https://help.ambire.com/hc/en-us/articles/22678327778460 '
 
 const TokenItem = ({ token }: { token: TokenResult }) => {
+  const { dispatch } = useBackgroundService()
   const { portfolio } = useSelectedAccountControllerState()
 
   const { networks } = useNetworksControllerState()
@@ -21,9 +26,47 @@ const TokenItem = ({ token }: { token: TokenResult }) => {
     simulatedAccountOp
   )
 
-  if (isProjectedRewards) return <ProjectedRewardsTokenItem token={token} />
-  if (isRewards) return <RewardsTokenItem token={token} />
-  if (isVesting) return <VestingTokenItem token={token} />
+  const handleDetailsPress = useCallback(() => {
+    window.open(INFO_BTN_URL, '_blank')
+  }, [])
+
+  const sendTransaction = useCallback(
+    (type: 'claimWalletRequest' | 'mintVestingRequest') => {
+      dispatch({
+        type: 'REQUESTS_CONTROLLER_BUILD_REQUEST',
+        params: { type, params: { token } }
+      })
+    },
+    [dispatch, token]
+  )
+
+  if (isProjectedRewards)
+    return (
+      <RewardsTokenItem
+        token={token}
+        onPress={handleDetailsPress}
+        actionButtonText="Info"
+        description="Projected rewards"
+      />
+    )
+  if (isRewards)
+    return (
+      <RewardsTokenItem
+        token={token}
+        onPress={() => sendTransaction('claimWalletRequest')}
+        actionButtonText="Claim"
+        description="Claimable rewards"
+      />
+    )
+  if (isVesting)
+    return (
+      <RewardsTokenItem
+        token={token}
+        actionButtonText="Claim"
+        onPress={() => sendTransaction('mintVestingRequest')}
+        description={!isPopup ? 'Claimable early supporters vestings' : 'Claimable vestings'}
+      />
+    )
 
   return <BaseTokenItem token={token} />
 }
