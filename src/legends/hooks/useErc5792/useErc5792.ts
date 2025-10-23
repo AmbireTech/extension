b392af6dabeed1b8b295ce17c184cafbe707bdef
Borislav Itskov <borislav.ickov@gmail.com>
2025-10-23 13:18:48 +0300

    walletSignUserOps helper in legends

--- src/legends/hooks/useErc5792/useErc5792.ts
+++ src/legends/hooks/useErc5792/useErc5792.ts
@@ -1,7 +1,13 @@
+import { ERC_4337_ENTRYPOINT } from '@ambire-common/consts/deploy'
+import { SignUserOperation } from '@ambire-common/interfaces/userOperation'
+import { GasSpeeds } from '@ambire-common/services/bundlers/types'
+import { getRpcProvider } from '@ambire-common/services/provider'
+import { executeBySenderInterface } from '@benzin/screens/BenzinScreen/constants/humanizerInterfaces'
 import { delayPromise } from '@common/utils/promises'
 import { RELAYER_URL } from '@env'
 import HumanReadableError from '@legends/classes/HumanReadableError'
 import useProviderContext from '@legends/hooks/useProviderContext'
+import { Interface, toBeHex } from 'ethers'
 
 export const ERRORS = {
   txFailed: 'tx-failed',
@@ -25,6 +31,18 @@ type Receipt = {
   }[]
   status: string
   transactionHash: string
+}
+
+const getGasPrice = async (chainId: bigint): Promise<GasSpeeds> => {
+  const url = `https://api.pimlico.io/v2/${chainId.toString()}/rpc?apikey=${
+    process.env.REACT_APP_PIMLICO_API_KEY
+  }`
+  const provider = getRpcProvider([url], chainId)
+  const prices: any = await provider.send('pimlico_getUserOperationGasPrice', [])
+  prices.medium = prices.standard
+  prices.ape = prices.fast
+  delete prices.standard
+  return prices
 }
 
 const useErc5792 = () => {
@@ -100,6 +118,92 @@ const useErc5792 = () => {
       )
 
     return receipt
+  }
+
+  const walletSignUserOps = async (
+    chainId: string,
+    accAddr: string,
+    calls: { to: string; data: string; value?: string }[]
+  ) => {
+    const baseSepoliaProvider: any = getRpcProvider(
+      ['https://sepolia.base.org'],
+      84532n,
+      'https://sepolia.base.org'
+    )
+    const opSepoliaProvider: any = getRpcProvider(
+      ['https://sepolia.optimism.io'],
+      11155420n,
+      'https://sepolia.optimism.io'
+    )
+    const entryPointInterface = new Interface([
+      'function getNonce(address, uint192) public view returns (uint256 nonce)'
+    ])
+    const entryPointNonce = await baseSepoliaProvider.call({
+      to: ERC_4337_ENTRYPOINT,
+      data: entryPointInterface.encodeFunctionData('getNonce', [accAddr, 0])
+    })
+    const nonce =
+      entryPointNonce && entryPointNonce !== '0x' ? toBeHex(BigInt(entryPointNonce)) : '0x00'
+    const entryPointNonceOp = await opSepoliaProvider.call({
+      to: ERC_4337_ENTRYPOINT,
+      data: entryPointInterface.encodeFunctionData('getNonce', [accAddr, 0])
+    })
+    const nonceOp =
+      entryPointNonceOp && entryPointNonceOp !== '0x' ? toBeHex(BigInt(entryPointNonceOp)) : '0x00'
+    const callData = executeBySenderInterface.encodeFunctionData(
+      'executeBySender',
+      calls.map((call) => {
+        return [[call.to, call.value?.toString() || 0, call.data]]
+      })
+    )
+    const baseSepoliaChainId = 84532n
+    const opSepoliaChainId = 11155420n
+    const gasPrice = await getGasPrice(baseSepoliaChainId)
+    const gasPriceOp = await getGasPrice(opSepoliaChainId)
+    const signUserOpsIdentifierJsonString: any = await provider!.request({
+      method: 'wallet_signUserOperations',
+      params: [
+        {
+          chainId: toBeHex(baseSepoliaChainId),
+          userOperation: {
+            sender: accAddr,
+            nonce,
+            callData,
+            callGasLimit: toBeHex(100000),
+            verificationGasLimit: toBeHex(100000),
+            preVerificationGas: toBeHex(100000),
+            maxFeePerGas: gasPrice.medium.maxFeePerGas,
+            maxPriorityFeePerGas: gasPrice.medium.maxPriorityFeePerGas
+          }
+        },
+        {
+          chainId: toBeHex(opSepoliaChainId),
+          userOperation: {
+            sender: accAddr,
+            nonce: nonceOp,
+            callData,
+            callGasLimit: toBeHex(100000),
+            verificationGasLimit: toBeHex(100000),
+            preVerificationGas: toBeHex(100000),
+            maxFeePerGas: gasPriceOp.medium.maxFeePerGas,
+            maxPriorityFeePerGas: gasPriceOp.medium.maxPriorityFeePerGas
+          }
+        }
+      ]
+    })
+    const signUserOpsIdentifier: { chainId: string; userOp: SignUserOperation }[] = JSON.parse(
+      signUserOpsIdentifierJsonString
+    )
+    for (let i = 0; i <= signUserOpsIdentifier.length; i++) {
+      const oneIdentifier = signUserOpsIdentifier[i]
+      // eslint-disable-next-line no-await-in-loop
+      const userOpHash = await provider!.request({
+        method: 'eth_sendRawUserOperation',
+        params: [oneIdentifier]
+      })
+    }
+
+    return signUserOpsIdentifierJsonString as string
   }
 
   return {
