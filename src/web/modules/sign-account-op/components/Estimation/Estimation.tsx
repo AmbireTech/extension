288052762bbe7f7f7a54def349a015cc3dc9fe41
Borislav Itskov <borislav.ickov@gmail.com>
2025-10-27 16:19:55 +0200

    add: a retry button for when the bundler estimation fails

--- src/web/modules/sign-account-op/components/Estimation/Estimation.tsx
+++ src/web/modules/sign-account-op/components/Estimation/Estimation.tsx
@@ -27,6 +27,7 @@ import ManifestImage from '@web/components/ManifestImage'
 import { openInTab } from '@web/extension-services/background/webapi/tab'
 import useBackgroundService from '@web/hooks/useBackgroundService'
 
+import Button from '@common/components/Button'
 import EstimationSkeleton from './components/EstimationSkeleton'
 import PayOption from './components/PayOption'
 import { NO_FEE_OPTIONS } from './consts'
@@ -552,10 +553,32 @@ const Estimation = ({
         </>
       )}
       {bundlerNonceDiscrepancy && (
-        <View style={[flexbox.directionRow, flexbox.alignEnd, spacings.mt]}>
+        <View
+          style={[
+            flexbox.directionRow,
+            spacings.mt,
+            flexbox.alignCenter,
+            flexbox.justifySpaceBetween
+          ]}
+        >
           <Text fontSize={12} appearance="warningText">
             {t(bundlerNonceDiscrepancy.title)}
           </Text>
+          <Button
+            type="warning"
+            text={t('Retry')}
+            onPress={() => {
+              dispatch({
+                type: 'SIGN_ACCOUNT_OP_REESTIMATE',
+                params: {
+                  type: signAccountOpState.type
+                }
+              })
+            }}
+            hasBottomSpacing={false}
+            size="tiny"
+            style={{ width: 98 }}
+          />
         </View>
       )}
     </>
