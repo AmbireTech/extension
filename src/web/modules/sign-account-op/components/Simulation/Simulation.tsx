d898a530acff48ebfb2a430649000f4c2139defd
Goran Stoyanov <gogo@goodmorning.dev>
2025-11-26 10:19:57 +0200

    add: period to transaction simulation warning

--- src/web/modules/sign-account-op/components/Simulation/Simulation.tsx
+++ src/web/modules/sign-account-op/components/Simulation/Simulation.tsx
@@ -345,7 +345,7 @@ const Simulation: FC<Props> = ({ network, isEstimationComplete, isViewOnly }) =>
       )}
 
       {simulationView === 'error' && (
-        <Alert type="error" title={t('Unable to simulate the transaction. Proceed with caution')} />
+        <Alert type="error" title={t('Unable to simulate the transaction. Proceed with caution.')} />
       )}
       {simulationView === 'no-changes' && (
         <View style={[flexbox.directionRow, flexbox.flex1, flexbox.alignCenter]}>
