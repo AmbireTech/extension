c4afeaf0c9e5d287f427216f979f6792a75844d1
Petromir Petrov <petromirdev@gmail.com>
2025-12-15 16:17:13 +0200

    rewards config: sentry to use git sha for release versioning

--- src/legends/instrument.ts
+++ src/legends/instrument.ts
@@ -2,18 +2,16 @@ import CONFIG from '@common/config/env'
 import { SENTRY_DSN_LEGENDS } from '@env'
 import * as Sentry from '@sentry/react'
 
-import packageJsonData from '../ambire-common/package.json'
+const version = process.env.GIT_COMMIT_SHA
 
-const version = packageJsonData.version
-
-if (SENTRY_DSN_LEGENDS)
+if (SENTRY_DSN_LEGENDS && version)
   Sentry.init({
     dsn: SENTRY_DSN_LEGENDS,
 
     // Adds request headers and IP for users, for more info visit:
     // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#sendDefaultPii
     sendDefaultPii: false,
-    release: `legends@${version}`,
+    release: version ? `legends@${version}` : undefined,
     integrations: [],
     environment: CONFIG.APP_ENV || 'unknown',
     beforeSend(event) {
