f8735539102a27b3360e5dec6aaa979793587ce2
Petromir Petrov <petromirdev@gmail.com>
2026-01-15 12:53:52 +0200

    fix: eslint flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,36 @@
 const ambireCommonConfig = require('./src/ambire-common/eslint.config.js')
 
-/** @type {import('eslint').Linter.FlatConfig} */
-module.exports = {
+/** @type {import('eslint').Linter.FlatConfig[]} */
+module.exports = [
+  // Spread the ambire-common config array
   ...ambireCommonConfig,
-  files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
-  languageOptions: {
-    parserOptions: {
-      project: './tsconfig.json'
+  // Add project-specific overrides
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json'
+      },
+      globals: {
+        browser: 'readonly',
+        node: true,
+        jest: true,
+        process: 'readonly',
+        chrome: 'readonly',
+        injectWeb3: 'readonly',
+        __dirname: 'readonly',
+        chromeTargetConfig: 'writable',
+        firefoxTargetConfig: 'writable',
+        Web3: true
+      }
     },
-    globals: {
-      browser: 'readonly',
-      node: true,
-      jest: true,
-      process: 'readonly',
-      chrome: 'readonly',
-      injectWeb3: 'readonly',
-      __dirname: 'readonly',
-      chromeTargetConfig: 'writable',
-      firefoxTargetConfig: 'writable',
-      Web3: true
+    rules: {
+      'import/extensions': 'off',
+      'class-methods-use-this': 'off',
+      'no-nested-ternary': 'off',
+      'prefer-promise-reject-errors': 'off',
+      'no-underscore-dangle': 'off',
+      'react/jsx-key': 'error'
     }
-  },
-  rules: {
-    'import/extensions': 'off',
-    'class-methods-use-this': 'off',
-    'no-nested-ternary': 'off',
-    'prefer-promise-reject-errors': 'off',
-    'no-underscore-dangle': 'off',
-    'react/jsx-key': 'error'
   }
-}
+]
