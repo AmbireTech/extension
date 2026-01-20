934a866c6afa211c7a5a05b91e57542036357056
Borislav Itskov <borislav.ickov@gmail.com>
2026-01-20 10:59:20 +0200

    remove the text sponsor logic

--- src/web/modules/sign-account-op/components/Estimation/components/Sponsored.tsx
+++ src/web/modules/sign-account-op/components/Estimation/components/Sponsored.tsx
@@ -1,28 +1,18 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { View } from 'react-native'
+
 import { Sponsor } from '@ambire-common/libs/erc7677/types'
 import ManifestFallbackIcon from '@common/assets/svg/ManifestFallbackIcon'
-import SuccessIcon from '@common/assets/svg/SuccessIcon'
 import Text from '@common/components/Text'
 import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
 import flexbox from '@common/styles/utils/flexbox'
 import ManifestImage from '@web/components/ManifestImage'
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import { View } from 'react-native'
 
 const Sponsored = ({ sponsor }: { sponsor?: Sponsor }) => {
   const { t } = useTranslation()
   const { theme } = useTheme()
-
-  if (sponsor && sponsor.text)
-    return (
-      <View style={[spacings.mt, flexbox.directionRow, flexbox.alignCenter]}>
-        <SuccessIcon color={theme.successDecorative} />
-        <Text fontSize={14} appearance="successText" style={spacings.mlTy}>
-          {sponsor.text}
-        </Text>
-      </View>
-    )
 
   return (
     <View>
