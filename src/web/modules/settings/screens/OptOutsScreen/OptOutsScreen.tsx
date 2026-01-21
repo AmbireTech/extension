c39002df043e5479f730daa919feb4b2cb4c3d27
Borislav Itskov <borislav.ickov@gmail.com>
2026-01-21 17:42:48 +0200

    use an universal OptOutControlOption for each opt out option

--- src/web/modules/settings/screens/OptOutsScreen/OptOutsScreen.tsx
+++ src/web/modules/settings/screens/OptOutsScreen/OptOutsScreen.tsx
@@ -1,14 +1,19 @@
 import React, { useContext, useEffect } from 'react'
+import { useTranslation } from 'react-i18next'
 import { View } from 'react-native'
 
+import HelpIcon from '@common/assets/svg/HelpIcon'
+import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
 import SettingsPageHeader from '@web/modules/settings/components/SettingsPageHeader'
 import { SettingsRoutesContext } from '@web/modules/settings/contexts/SettingsRoutesContext'
 
-import DefiPositionsControlOption from '../GeneralSettingsScreen/components/DefiPositionsControlOption'
+import OptOutControlOption from './components/OptOutControlOption'
 
 const OptOutsScreen = () => {
   const { setCurrentSettingsPage } = useContext(SettingsRoutesContext)
+  const { t } = useTranslation()
+  const { theme } = useTheme()
 
   useEffect(() => {
     setCurrentSettingsPage('opt-outs')
@@ -18,7 +23,12 @@ const OptOutsScreen = () => {
     <>
       <SettingsPageHeader title="Privacy opt outs" />
       <View style={spacings.mb2Xl}>
-        <DefiPositionsControlOption />
+        <OptOutControlOption
+          title={t('Defi positions')}
+          description={t('Should we fetch your defi positions.')}
+          icon={<HelpIcon color={theme.primaryText} />}
+          flag="defiPositions"
+        />
       </View>
     </>
   )
