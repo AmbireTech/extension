f7dc49f727d0bb945e8e49a25972b0a362aa00b5
sonytooo <simeonpetkov11999977@gmail.com>
2026-02-02 13:52:27 +0200

    ethereum inpage optimizations

--- src/web/extension-services/inpage/ethereum-inpage.ts
+++ src/web/extension-services/inpage/ethereum-inpage.ts
@@ -8,37 +8,12 @@ if (/Opera|OPR\//i.test(navigator.userAgent)) {
   const d = Object.getOwnPropertyDescriptor(window, 'ethereum')
   let isDapp = false
 
-  // Create a proxied provider that detects actual usage of the provider
-  const createMonitoredProvider = () => {
+  // Get a proxied provider that detects actual usage of the provider
+  const getMonitoredProvider = () => {
     const provider = globalIsAmbireNext ? window.ambireNext : window.ambire
 
     return new Proxy(provider, {
       get(target, prop, receiver) {
-        const value = Reflect.get(target, prop, receiver)
-
-        isDapp = true
-
-        // Notify background that this is a real dApp
-        if (provider) {
-          // eslint-disable-next-line @typescript-eslint/no-floating-promises
-          provider
-            .request({
-              method: 'eth_chainId',
-              params: []
-            })
-            .catch((e) => console.error('Error notifying background of window.ethereum usage', e))
-        }
-
-        return value
-      }
-    })
-  }
-
-  if (!d || d.configurable) {
-    Object.defineProperty(window, 'ethereum', {
-      configurable: false,
-      enumerable: true,
-      get: () => {
         if (!isDapp) {
           try {
             // throw an Error to determine the source of the request
@@ -48,17 +23,31 @@ if (/Opera|OPR\//i.test(navigator.userAgent)) {
             if (stack) {
               const callerPage = (typeof stack === 'string' && stack.split('\n')[2]?.trim()) || ''
               if (callerPage.includes(window.location.hostname)) {
-                // Yes, the app is using window.ethereum, but we need to make sure
-                // that it's actually calling methods on it. This is because some websites
-                // (like amazon) just check for its presence for tracking purposes.
-                return createMonitoredProvider()
+                isDapp = true
+
+                // Notify background that this is a real dApp
+                if (provider) {
+                  provider
+                    .request({ method: 'eth_chainId', params: [] })
+                    .catch((e) =>
+                      console.error('Error notifying background of window.ethereum usage', e)
+                    )
+                }
               }
             }
           }
         }
 
-        return globalIsAmbireNext ? window.ambireNext : window.ambire
-      },
+        return Reflect.get(target, prop, receiver)
+      }
+    })
+  }
+
+  if (!d || d.configurable) {
+    Object.defineProperty(window, 'ethereum', {
+      configurable: false,
+      enumerable: true,
+      get: getMonitoredProvider,
       set: () => {}
     })
   }
