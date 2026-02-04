13abde614edbed6fcf5b8c9549e943fe223fe014
Kaloyan Kosev <superkalo@goodmorning.dev>
2026-02-04 14:13:07 +0200

    Fix: ignore mismatch between types for FatToggle

--- src/common/components/FatToggle/FatToggle.tsx
+++ src/common/components/FatToggle/FatToggle.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { DimensionValue, ViewStyle } from 'react-native'
 
 import Toggle from '@common/components/Toggle'
 import { ToggleProps } from '@common/components/Toggle/types'
@@ -11,22 +12,24 @@ const FatToggle: React.FC<ToggleProps> = (props) => {
   return (
     <Toggle
       {...props}
-      trackStyle={{
-        width: 52,
-        height: 28,
-        // @ts-ignore mismatch between types
-        borderRadius: 16,
-        ...props.trackStyle
-      }}
-      toggleStyle={{
-        top: 2,
-        width: 24,
-        height: 24,
-        transform: (props.isOn ? 'translateX(26px)' : 'translateX(2px)') as any,
-        // @ts-ignore mismatch between types
-        border: `1px solid ${theme.secondaryBorder as string}`,
-        ...props.toggleStyle
-      }}
+      trackStyle={
+        {
+          width: 52,
+          height: 28,
+          borderRadius: 16,
+          ...props.trackStyle
+        } as ViewStyle // ignore mismatch between types
+      }
+      toggleStyle={
+        {
+          top: 2,
+          width: 24,
+          height: 24,
+          transform: (props.isOn ? 'translateX(26px)' : 'translateX(2px)') as any,
+          border: `1px solid ${theme.secondaryBorder as string}`,
+          ...props.toggleStyle
+        } as ViewStyle // ignore mismatch between types
+      }
     />
   )
 }
