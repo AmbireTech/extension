9e2aec2d3c2347289c87d1c82159bd8b9b133b81
Petromir Petrov <petromirdev@gmail.com>
2026-02-05 13:54:00 +0200

    redesign: HW icons

--- src/common/components/AccountKeyIcon/AccountKeyIcon.tsx
+++ src/common/components/AccountKeyIcon/AccountKeyIcon.tsx
@@ -7,17 +7,20 @@ import NoKeysIcon from '@common/assets/svg/NoKeysIcon'
 import SingleKeyIcon from '@common/assets/svg/SingleKeyIcon'
 import TrezorLockIcon from '@common/assets/svg/TrezorLockIcon'
 import { KeyType } from '@common/components/AccountKeyIcons/AccountKeyIcons'
-import useTheme from '@common/hooks/useTheme'
 
 const AccountKeyIcon = ({ type, color }: { type: KeyType; color?: string | ColorValue }) => {
-  const { theme } = useTheme()
+  const props = {
+    color,
+    width: 16,
+    height: 16
+  }
 
-  if (type === 'lattice') return <LatticeIcon color={color} width={32} height={32} />
-  if (type === 'trezor') return <TrezorLockIcon color={color} width={20} height={20} />
-  if (type === 'ledger') return <LedgerLetterIcon color={color} width={20} height={20} />
-  if (type === 'none') return <NoKeysIcon color={theme.secondaryText} width={20} height={20} />
+  if (type === 'lattice') return <LatticeIcon {...props} />
+  if (type === 'trezor') return <TrezorLockIcon {...props} />
+  if (type === 'ledger') return <LedgerLetterIcon {...props} />
+  if (type === 'none') return <NoKeysIcon {...props} />
 
-  return <SingleKeyIcon color={color} width={16} height={16} />
+  return <SingleKeyIcon {...props} />
 }
 
 export default React.memo(AccountKeyIcon)
