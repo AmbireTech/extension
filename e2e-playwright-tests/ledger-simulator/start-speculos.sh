2918c638778edc9130319337d32075b5df7d1084
stojnovsky <stojnovsky@gmail.com>
2026-02-09 11:27:40 +0200

    added SEED from env for ledger emulator

--- e2e-playwright-tests/ledger-simulator/start-speculos.sh
+++ e2e-playwright-tests/ledger-simulator/start-speculos.sh
@@ -1,3 +1,3 @@
 docker pull ghcr.io/ledgerhq/speculos:0.25.13 && \
 docker image tag ghcr.io/ledgerhq/speculos:0.25.13 speculos && \
-docker run --rm -it --net=host -v "$(pwd)"/e2e-playwright-tests/ledger-simulator/apps:/speculos/apps speculos --model nanosp ./apps/app.elf --display headless --apdu-port 9999 --api-port 5000 --transport HID --seed "crater pear price rose dirt coffee fun blur spice uniform novel dinner"
+docker run --rm -it -p 9999:9999 -p 5000:5000 -v "$(pwd)"/e2e-playwright-tests/ledger-simulator/apps:/speculos/apps speculos --model nanosp ./apps/app.elf --display headless --apdu-port 9999 --api-port 5000 --transport HID --seed "$SEED"
