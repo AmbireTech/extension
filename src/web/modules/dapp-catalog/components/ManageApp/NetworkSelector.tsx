bb1d819bd2846bab5be59397925e6cae837924e1
Petromir Petrov <petromirdev@gmail.com>
2026-02-10 17:01:52 +0200

    remove: networkselector animation

--- src/web/modules/dapp-catalog/components/ManageApp/NetworkSelector.tsx
+++ src/web/modules/dapp-catalog/components/ManageApp/NetworkSelector.tsx
@@ -71,8 +71,6 @@ const NetworkSelector = ({ dapp, isAbove = false }: { dapp: Dapp; isAbove?: bool
   const { theme } = useTheme()
   const { t } = useTranslation()
 
-  const heightAnim = useRef(new Animated.Value(0)).current
-
   const networkData = networks.filter((n) => Number(n.chainId) === dapp.chainId)[0]
 
   const [bindAnim, animStyle] = useCustomHover({
@@ -97,33 +95,19 @@ const NetworkSelector = ({ dapp, isAbove = false }: { dapp: Dapp; isAbove?: bool
     [dispatch, dapp.id]
   )
 
-  useEffect(() => {
-    Animated.spring(heightAnim, {
-      toValue: isExpanded ? 1 : 0,
-      useNativeDriver: false,
-      tension: 80,
-      friction: 12
-    }).start()
-  }, [isExpanded, heightAnim])
-
-  const maxHeight = heightAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: [0, 320]
-  })
-
   const networkList = (
-    <Animated.View style={{ maxHeight, overflow: 'hidden' }}>
-      <ScrollView style={{ maxHeight: 320 }}>
-        {networks.map((network) => (
-          <NetworkOption
-            onSelectNetwork={onSelectNetwork}
-            key={network.chainId}
-            network={network}
-            isSelectedNetwork={dapp.chainId === Number(network.chainId)}
-          />
-        ))}
-      </ScrollView>
-    </Animated.View>
+    // I've been unable to animate the maxHeight of the container without causing
+    // jittering of the other elements on Firefox.
+    <ScrollView style={{ maxHeight: isExpanded ? 320 : 0 }}>
+      {networks.map((network) => (
+        <NetworkOption
+          onSelectNetwork={onSelectNetwork}
+          key={network.chainId}
+          network={network}
+          isSelectedNetwork={dapp.chainId === Number(network.chainId)}
+        />
+      ))}
+    </ScrollView>
   )
 
   return (
