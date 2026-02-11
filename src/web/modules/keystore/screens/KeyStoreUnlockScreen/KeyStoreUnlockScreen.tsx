fec5ee4b1b4fddcda347bbe3cc1919fad20a9035
Petromir Petrov <petromirdev@gmail.com>
2026-02-11 17:23:23 +0200

    fix: projected rewards displayed on the dashboard

--- src/web/modules/keystore/screens/KeyStoreUnlockScreen/KeyStoreUnlockScreen.tsx
+++ src/web/modules/keystore/screens/KeyStoreUnlockScreen/KeyStoreUnlockScreen.tsx
@@ -15,6 +15,7 @@ import useNavigation from '@common/hooks/useNavigation'
 import useTheme from '@common/hooks/useTheme'
 import { ROUTES } from '@common/modules/router/constants/common'
 import spacings from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 import text from '@common/styles/utils/text'
 import { DEFAULT_KEYSTORE_PASSWORD_DEV } from '@env'
 import LayoutWrapper from '@web/components/LayoutWrapper'
@@ -124,7 +125,7 @@ const KeyStoreUnlockScreen = () => {
               value={value}
               onSubmitEditing={handleSubmit((data) => handleUnlock(data))}
               error={passwordFieldError}
-              containerStyle={spacings.mb}
+              containerStyle={{ ...spacings.mb, ...flexbox.flex1 }}
             />
           )}
           name="password"
