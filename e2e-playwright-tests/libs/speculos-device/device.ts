d17c55f8a8e4ce56e89fabb965fa5d59fdeec59a
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2026-02-12 13:51:26 +0200

    Modified Speculos lib to get simulator events

--- e2e-playwright-tests/libs/speculos-device/device.ts
+++ e2e-playwright-tests/libs/speculos-device/device.ts
@@ -1,6 +1,7 @@
 import { LedgerSimulatorClient } from './client'
-import { Button, ButtonAction, SpeculosClientOptions } from './types'
+import { Button, ButtonAction, SpeculosClientOptions, SpeculosEvent } from './types'
 
+// resource: https://petstore.swagger.io/?url=https://raw.githubusercontent.com/LedgerHQ/speculos/master/speculos/api/static/swagger/swagger.json#/default/get_events
 export class SpeculosDevice {
   private client: LedgerSimulatorClient
 
@@ -22,5 +23,33 @@ export class SpeculosDevice {
 
   pressBothButtons() {
     return this.pressButton('both')
+  }
+
+  async getEvents() {
+    return this.client.getJson<{ events: SpeculosEvent[] }>('/events')
+  }
+
+  async resetEvents(): Promise<void> {
+    await this.client.delete('/events')
+  }
+
+  async waitForEvent(
+    predicate: (event: SpeculosEvent) => boolean,
+    timeout = 5000000
+  ): Promise<SpeculosEvent> {
+    const startTime = Date.now()
+
+    while (Date.now() - startTime < timeout) {
+      const { events } = await this.getEvents()
+      console.log('AllFound events:', events)
+      const event = events.find(predicate)
+      if (event) {
+        console.log('Found event:', event)
+        return event
+      }
+      await new Promise((resolve) => setTimeout(resolve, 500))
+    }
+
+    throw new Error('Timeout waiting for event')
   }
 }
