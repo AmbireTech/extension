f3af26eb124c32a7a1b5e2bbb5fd03715fd4f622
sonytooo <simeonpetkov11999977@gmail.com>
2026-02-27 16:24:19 +0200

    style and move in hooks the common logic for create new acc

---
+++ src/common/modules/auth/hooks/useCreateSeedWrite/useCreateSeedWrite.ts
@@ -0,0 +1,90 @@
+import { useCallback, useEffect, useMemo, useState } from 'react'
+
+import { KeystoreSeed } from '@ambire-common/interfaces/keystore'
+import { useTranslation } from '@common/config/localization'
+import useController from '@common/hooks/useController'
+import useControllersMiddleware from '@common/hooks/useControllersMiddleware'
+import useToast from '@common/hooks/useToast'
+import useOnboardingNavigation from '@common/modules/auth/hooks/useOnboardingNavigation'
+import eventBus from '@common/services/event/eventBus'
+import { setStringAsync } from '@common/utils/clipboard'
+
+export default function useCreateSeedWrite() {
+  const { goToNextRoute } = useOnboardingNavigation()
+  const { t } = useTranslation()
+  const { addToast } = useToast()
+  const { dispatch } = useControllersMiddleware()
+  const {
+    state: { hasTempSeed },
+    dispatch: keystoreDispatch
+  } = useController('KeystoreController')
+  const [tempSeed, setTempSeed] = useState<KeystoreSeed | null>(null)
+  const { initParams, subType } = useController('AccountPickerController').state
+  const [submitButtonPressed, setSubmitButtonPressed] = useState(false)
+
+  useEffect(() => {
+    keystoreDispatch({
+      type: 'method',
+      params: {
+        method: 'sendTempSeedToUi',
+        args: []
+      }
+    })
+  }, [keystoreDispatch])
+
+  useEffect(() => {
+    const onReceiveOneTimeData = (data: any) => {
+      if (!data.tempSeed) return
+
+      setTempSeed(data.tempSeed)
+    }
+
+    eventBus.addEventListener('receiveOneTimeData', onReceiveOneTimeData)
+
+    return () => eventBus.removeEventListener('receiveOneTimeData', onReceiveOneTimeData)
+  }, [])
+
+  const handleSubmit = useCallback(async () => {
+    if (!tempSeed) return
+
+    setSubmitButtonPressed(true)
+    dispatch({
+      type: 'MAIN_CONTROLLER_ACCOUNT_PICKER_INIT_PRIVATE_KEY_OR_SEED_PHRASE',
+      params: { privKeyOrSeed: tempSeed.seed, hdPathTemplate: tempSeed.hdPathTemplate }
+    })
+  }, [dispatch, tempSeed])
+
+  useEffect(() => {
+    if (!tempSeed) return
+    if (!!submitButtonPressed && initParams && subType === 'seed') {
+      setSubmitButtonPressed(false)
+      goToNextRoute()
+    }
+  }, [goToNextRoute, dispatch, tempSeed, initParams, submitButtonPressed, subType])
+
+  const handleCopyToClipboard = useCallback(async () => {
+    try {
+      if (!tempSeed) return
+
+      await setStringAsync(tempSeed.seed)
+      addToast(t('Recovery phrase copied to clipboard'))
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.error(error)
+      addToast(t('Failed to copy recovery phrase'))
+    }
+  }, [addToast, tempSeed, t])
+
+  const seedArray = useMemo(() => {
+    if (!tempSeed) return []
+
+    return tempSeed.seed.split(' ')
+  }, [tempSeed])
+
+  return {
+    handleSubmit,
+    handleCopyToClipboard,
+    seedArray,
+    submitButtonPressed
+  }
+}
