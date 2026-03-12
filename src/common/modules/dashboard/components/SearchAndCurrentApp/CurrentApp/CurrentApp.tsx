8e535a574677f03e3d5e330c117bef1686d523ec
Petromir Petrov <petromirdev@gmail.com>
2026-03-12 11:05:37 +0200

    update: default cursor if dapp is not connected

--- src/common/modules/dashboard/components/SearchAndCurrentApp/CurrentApp/CurrentApp.tsx
+++ src/common/modules/dashboard/components/SearchAndCurrentApp/CurrentApp/CurrentApp.tsx
@@ -1,8 +1,9 @@
 import React, { useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Pressable, View } from 'react-native'
+import { Pressable, View, ViewStyle } from 'react-native'
 
 import { createGlobalTooltipDataSet } from '@common/components/GlobalTooltip'
+import { isWeb } from '@common/config/env'
 import useController from '@common/hooks/useController'
 import useTheme from '@common/hooks/useTheme'
 import DappIcon from '@common/modules/dapp-catalog/components/DappIcon'
@@ -40,7 +41,8 @@ const CurrentApp = () => {
           backgroundColor: !isBlacklisted ? theme.primaryBackground : theme.errorBackground,
           borderRadius: 20,
           ...spacings.ml,
-          ...flexbox.center
+          ...flexbox.center,
+          ...(isWeb && !currentDapp.isConnected ? ({ cursor: 'default' } as any as ViewStyle) : {})
         }}
         dataSet={
           isBlacklisted
