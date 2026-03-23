9cd76f361a420a9c96fb42dbf357954ed760ae53
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2026-03-23 13:19:33 +0200

    Fixed QrSignRequestScreen styles

--- src/web/modules/hardware-wallet/screens/QrSignRequestScreen/QrSignRequestScreen.tsx
+++ src/web/modules/hardware-wallet/screens/QrSignRequestScreen/QrSignRequestScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { View } from 'react-native'
 
 import Button from '@common/components/Button'
-import Panel, { PanelProps } from '@common/components/Panel'
+import FooterGlassView from '@common/components/FooterGlassView'
 import Text from '@common/components/Text'
 import { useTranslation } from '@common/config/localization'
 import spacings from '@common/styles/spacings'
@@ -14,32 +14,36 @@ type Props = {
   onReject: () => void
   urType: string | undefined
   urCborHex: any
-  panelType?: PanelProps['type']
 }
 
 const FRAME_INTERVAL = 300
 
-const QrSignRequestScreen = ({
-  onContinue,
-  onReject,
-  urType,
-  urCborHex,
-  panelType = 'default'
-}: Props) => {
+const QrSignRequestScreen = ({ onContinue, onReject, urType, urCborHex }: Props) => {
   const { t } = useTranslation()
 
   return (
-    <Panel type={panelType} title={t('Scan with your hardware wallet')} withBackButton={false}>
-      <Text style={spacings.mbTy}>
-        {t('Scan this QR code with your Keystone device to sign the message.')}
-      </Text>
+    <View style={flexbox.center}>
+      <Text style={spacings.mbTy}>{t('Scan this QR code with your QR-based device to sign.')}</Text>
       <View style={[flexbox.center, spacings.mtSm]}>
         <AnimatedQRCode options={{ size: 300 }} type={urType} cbor={urCborHex} />
-        {/* TODO: Fix the styles */}
-        <Button style={spacings.mtTy} text={t('I have scanned the QR code')} onPress={onContinue} />
-        <Button text={t('Reject')} onPress={onReject} />
+        <FooterGlassView size="sm" absolute={false} style={spacings.pt}>
+          <Button
+            size="smaller"
+            hasBottomSpacing={false}
+            type="secondary"
+            text={t('Back')}
+            onPress={onReject}
+            style={spacings.mrSm}
+          />
+          <Button
+            size="smaller"
+            hasBottomSpacing={false}
+            text={t('Get signature')}
+            onPress={onContinue}
+          />
+        </FooterGlassView>
       </View>
-    </Panel>
+    </View>
   )
 }
 
