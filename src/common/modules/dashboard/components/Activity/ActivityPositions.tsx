adf05303807303b171d4e613250a062d22321a9f
sonytooo <simeonpetkov11999977@gmail.com>
2026-03-24 14:20:11 +0200

    text align fixes

--- src/common/modules/dashboard/components/Activity/ActivityPositions.tsx
+++ src/common/modules/dashboard/components/Activity/ActivityPositions.tsx
@@ -182,7 +182,11 @@ const ActivityPositions: FC<Props> = ({
               {t(
                 `Ambire doesn't retrieve transactions made${isWeb ? '\n' : ''} before installing the extension, but you can ${isWeb ? '\n' : ''}check your address on `
               )}
-              <TouchableOpacity
+              <Text
+                weight="medium"
+                color={theme.linkText}
+                fontSize={16}
+                style={{ textDecorationLine: 'none' }}
                 onPress={() =>
                   openInTab({
                     url: blockExplorerUrl(
@@ -193,10 +197,8 @@ const ActivityPositions: FC<Props> = ({
                   })
                 }
               >
-                <Text weight="medium" color={theme.linkText} style={{ textDecorationLine: 'none' }}>
-                  {blockExplorerName(network?.explorerUrl || 'https://etherscan.io')}
-                </Text>
-              </TouchableOpacity>
+                {blockExplorerName(network?.explorerUrl || 'https://etherscan.io')}
+              </Text>
               .
             </Text>
           </View>
