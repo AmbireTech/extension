a781b913ce790c157e9ea50a6e977de792851ca0
Lyubomir Nanev <lyubomir.nanev@gmail.com>
2026-04-21 11:28:22 +0300

    Removed walletType restrictions

--- src/web/modules/hardware-wallet/qr/wallets/ImTokenQrWallet.ts
+++ src/web/modules/hardware-wallet/qr/wallets/ImTokenQrWallet.ts
@@ -1,12 +1,8 @@
-import { BIP44_STANDARD_DERIVATION_TEMPLATE } from '@ambire-common/consts/derivation'
 import { QrWalletConfig } from '@ambire-common/interfaces/keyIterator'
 
 export const ImTokenQrWallet: QrWalletConfig = {
-  walletType: 'imtoken' as const,
   protocol: 'ur' as const,
   label: 'imToken',
-  hdPathTemplate: BIP44_STANDARD_DERIVATION_TEMPLATE,
-  relativePathTemplate: '<account>',
   tutorialUrl:
     'https://help.ambire.com/en/articles/14483029-how-to-connect-ambire-wallet-with-imtoken'
 }
