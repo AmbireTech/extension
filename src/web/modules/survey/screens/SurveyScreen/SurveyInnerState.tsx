eeac5c835b87a42063dfb37ce917708a89ec3e6b
Yosif Hamed <josif.hameed@gmail.com>
2026-04-21 13:19:33 +0300

    refactor(survey): use switch instead of if-else

--- src/web/modules/survey/screens/SurveyScreen/SurveyInnerState.tsx
+++ src/web/modules/survey/screens/SurveyScreen/SurveyInnerState.tsx
@@ -31,76 +31,93 @@ const SurveyInnerState = ({
 }: Props) => {
   const { theme } = useTheme()
   const { t } = useTranslation()
-  if (surveyStatus === 'not-started') return null
-  else if (surveyStatus === 'loading-fetching')
-    return <InProgress title={t('Getting the survey...')} />
-  else if (surveyStatus === 'success-fetched')
-    return (
-      currentQuestion && (
-        <View>
-          <Text fontSize={SPACING_MD} weight={'medium'} appearance="primaryText">
-            {currentQuestion.text}
-          </Text>
-          {currentQuestion.responseType === 'singleChoice' && currentQuestion.responseOptions ? (
-            <View style={[spacings.mtMd, spacings.mlLg]}>
-              <RadioQuestions
-                selectedResponseId={typeof inputtedAnswer === 'string' ? null : inputtedAnswer}
-                setSelectedResponseId={setInputtedAnswer}
-                responses={currentQuestion.responseOptions}
+
+  switch (surveyStatus) {
+    case 'not-started':
+      return null
+
+    case 'loading-fetching':
+      return <InProgress title={t('Getting the survey...')} />
+
+    case 'success-fetched':
+      return (
+        currentQuestion && (
+          <View>
+            <Text fontSize={SPACING_MD} weight="medium" appearance="primaryText">
+              {currentQuestion.text}
+            </Text>
+            {currentQuestion.responseType === 'singleChoice' && currentQuestion.responseOptions ? (
+              <View style={[spacings.mtMd, spacings.mlLg]}>
+                <RadioQuestions
+                  selectedResponseId={typeof inputtedAnswer === 'string' ? null : inputtedAnswer}
+                  setSelectedResponseId={setInputtedAnswer}
+                  responses={currentQuestion.responseOptions}
+                />
+              </View>
+            ) : (
+              <TextArea
+                placeholder="Write down your thoughts..."
+                value={inputtedAnswer?.toString() || ''}
+                onChangeText={setInputtedAnswer}
+                containerStyle={spacings.ptLg}
+                inputWrapperStyle={{
+                  height: 100,
+                  borderColor: theme.neutral600,
+                  borderWidth: 1,
+                  alignItems: 'baseline'
+                }}
+                multiline
               />
-            </View>
-          ) : (
-            <TextArea
-              placeholder="Write down your thoughts..."
-              value={inputtedAnswer?.toString() || ''}
-              onChangeText={setInputtedAnswer}
-              containerStyle={spacings.ptLg}
-              inputWrapperStyle={{
-                height: 100,
-                borderColor: theme.neutral600,
-                borderWidth: 1,
-                alignItems: 'baseline'
-              }}
-              multiline
-            />
-          )}
-        </View>
+            )}
+          </View>
+        )
       )
-    )
-  else if (surveyStatus === 'loading-sending')
-    return (
-      <Completed
-        title={t('Thank you!')}
-        titleSecondary={t('Your answers are being sent.')}
-        isLoading
-        openExplorerText={''}
-      />
-    )
-  else if (surveyStatus === 'success-submitted')
-    return (
-      <Completed
-        title={t('Thank you!')}
-        titleSecondary={t('Your answers have been sent successfully.')}
-        openExplorerText={''}
-      />
-    )
-  else if (surveyStatus === 'error-submitting')
-    return (
-      <Failed
-        alertStyle={{ maxWidth: '100%' }}
-        title={t('We failed to send your response.')}
-        errorMessage={`Error: ${errorMessage || 'unknown submitting error'}`}
-      />
-    )
-  // if (surveyStatus === 'error-fetching')
-  else
-    return (
-      <Failed
-        alertStyle={{ maxWidth: '100%' }}
-        title={t('We failed to fetch the survey.')}
-        errorMessage={`Error: ${errorMessage || 'unknown fetching error'}`}
-      />
-    )
+
+    case 'loading-sending':
+      return (
+        <Completed
+          title={t('Thank you!')}
+          titleSecondary={t('Your answers are being sent.')}
+          isLoading
+          openExplorerText=""
+        />
+      )
+
+    case 'success-submitted':
+      return (
+        <Completed
+          title={t('Thank you!')}
+          titleSecondary={t('Your answers have been sent successfully.')}
+          openExplorerText=""
+        />
+      )
+
+    case 'error-submitting':
+      return (
+        <Failed
+          alertStyle={{ maxWidth: '100%' }}
+          title={t('We failed to send your response.')}
+          errorMessage={`Error: ${errorMessage || 'unknown submitting error'}`}
+        />
+      )
+
+    case 'error-fetching':
+      return (
+        <Failed
+          alertStyle={{ maxWidth: '100%' }}
+          title={t('We failed to fetch the survey.')}
+          errorMessage={`Error: ${errorMessage || 'unknown fetching error'}`}
+        />
+      )
+    default:
+      return (
+        <Failed
+          alertStyle={{ maxWidth: '100%' }}
+          title={t('Internal error, contact support and restart the extension.')}
+          errorMessage={`Error: unexpected survey state ${surveyStatus}}`}
+        />
+      )
+  }
 }
 
 export default React.memo(SurveyInnerState)
