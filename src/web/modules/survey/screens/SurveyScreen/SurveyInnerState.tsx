3c5edd0c887068a993ffb5538a5270e8679f7cde
Yosif Hamed <josif.hameed@gmail.com>
2026-04-22 13:42:57 +0300

    change(survey): text inputs to grow in height

--- src/web/modules/survey/screens/SurveyScreen/SurveyInnerState.tsx
+++ src/web/modules/survey/screens/SurveyScreen/SurveyInnerState.tsx
@@ -56,17 +56,20 @@ const SurveyInnerState = ({
               </View>
             ) : (
               <TextArea
+                // the key is added so the component remounts and resets its height when the
+                // question changes
+                key={currentQuestion.id}
                 placeholder="Write down your thoughts..."
                 value={inputtedAnswer?.toString() || ''}
                 onChangeText={setInputtedAnswer}
                 containerStyle={spacings.ptLg}
                 inputWrapperStyle={{
-                  height: 100,
                   borderColor: theme.neutral600,
                   borderWidth: 1,
                   alignItems: 'baseline'
                 }}
                 multiline
+                enableGrowHeightRange={[100, 200]}
               />
             )}
           </View>
