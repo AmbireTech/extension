73a99ce153498e396e6ebbf5f2e5a50e11197544
Filipv95 <filipv65@gmail.com>
2026-04-22 17:49:56 +0200

    skip ambire rewards tests

--- e2e-playwright-tests/tests/ambireRewards/ambireRewards.spec.ts
+++ e2e-playwright-tests/tests/ambireRewards/ambireRewards.spec.ts
@@ -5,7 +5,8 @@ import { ambireRewardsText } from 'pages/utils/data/ambireRewardsText'
 
 import { expect, Page } from '@playwright/test'
 
-test.describe('ambire rewards', { tag: '@rewards' }, () => {
+// TODO: skipping until further notice - rewards are temporarily disabled
+test.describe.skip('ambire rewards', { tag: '@rewards' }, () => {
   test.setTimeout(80000)
 
   test.beforeEach(async ({ pages }) => {
@@ -16,7 +17,8 @@ test.describe('ambire rewards', { tag: '@rewards' }, () => {
     await context.close()
   })
 
-  test('Check redirection to Ambire rewards page', async ({ pages }) => {
+  test.only('Check redirection to Ambire rewards page', async ({ pages }) => {
+    await pages.auth.pause()
     await test.step('assert rewards button is visible', async () => {
       await pages.basePage.isVisible(selectors.dashboard.rewardsButton)
     })
