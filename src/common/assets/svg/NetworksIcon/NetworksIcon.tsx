58f53cd36097e9006c0da7462a9d887fe1f092fa
Kaloyan Kosev <superkalo@goodmorning.dev>
2026-04-28 15:48:57 +0300

    Change: tweak the icon sizing

--- src/common/assets/svg/NetworksIcon/NetworksIcon.tsx
+++ src/common/assets/svg/NetworksIcon/NetworksIcon.tsx
@@ -7,18 +7,18 @@ const NetworksIcon: React.FC<SvgProps> = ({ width = 24, height = 24, color }) =>
   const { theme } = useTheme()
 
   return (
-    <Svg width={width} height={height} viewBox="0 0 21.5 21.5" fill="none">
-      <Circle cx="10.75" cy="10.75" r="10" stroke={color || theme.iconPrimary} strokeWidth="1.5" />
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Circle cx="12" cy="12" r="9" stroke={color || theme.iconPrimary} strokeWidth="1.5" />
       <Ellipse
-        cx="10.7498"
-        cy="10.75"
-        rx="3.33333"
-        ry="10"
+        cx="12"
+        cy="12"
+        rx="3"
+        ry="9"
         stroke={color || theme.iconPrimary}
         strokeWidth="1.5"
       />
       <Path
-        d="M0.75 10.75H20.75"
+        d="M3 12H21"
         stroke={color || theme.iconPrimary}
         strokeWidth="1.5"
         strokeLinecap="round"
