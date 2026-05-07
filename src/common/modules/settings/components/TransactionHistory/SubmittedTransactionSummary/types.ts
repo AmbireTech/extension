f3d743f342aa83e2edec5e21530f0e3cbea559f2
Borislav Itskov <borislav@devlabs.bg>
2026-05-07 22:32:20 +0300

    move type declarations to common

--- src/common/modules/settings/components/TransactionHistory/SubmittedTransactionSummary/types.ts
+++ src/common/modules/settings/components/TransactionHistory/SubmittedTransactionSummary/types.ts
@@ -1,40 +1,11 @@
 import { ViewStyle } from 'react-native'
 
-import { BalanceChange, SubmittedAccountOp } from '@ambire-common/libs/accountOp/submittedAccountOp'
+import {
+  BalanceChange,
+  SubmittedAccountOpLike
+} from '@ambire-common/libs/accountOp/submittedAccountOp'
 
-type SubmittedAccountOpActionFields = Pick<
-  SubmittedAccountOp,
-  | 'signingKeyAddr'
-  | 'signingKeyType'
-  | 'nonce'
-  | 'eoaNonce'
-  | 'feeCall'
-  | 'activatorCall'
-  | 'gasLimit'
-  | 'signature'
-  | 'asUserOperation'
-  | 'signers'
-  | 'signed'
-  | 'safeTx'
-  | 'flags'
->
-
-export interface SubmittedAccountOpLike
-  extends Pick<
-      SubmittedAccountOp,
-      | 'id'
-      | 'accountAddr'
-      | 'chainId'
-      | 'calls'
-      | 'gasFeePayment'
-      | 'txnId'
-      | 'status'
-      | 'meta'
-      | 'timestamp'
-      | 'identifiedBy'
-      | 'balanceChanges'
-    >,
-    Partial<SubmittedAccountOpActionFields> {}
+export type { SubmittedAccountOpLike }
 
 export interface Props {
   submittedAccountOp: SubmittedAccountOpLike
