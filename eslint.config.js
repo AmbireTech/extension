42b8df2389e3eff5c0492896da64feb0ea7824bf
Kaloyan Kosev <superkalo@goodmorning.dev>
2026-05-11 13:09:53 +0300

    Changed: Update ESLint and all its deps dependencies to latest v9 versions; Refactor: Consolidate ESLint ignore rules into eslint.config.js (separate ignore file is deprecated in v9)

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,28 @@ const ambireCommonConfig = require('./src/ambire-common/eslint.config.js')
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 module.exports = [
+  {
+    ignores: [
+      'node_modules/**',
+      'ios/**',
+      'android/**',
+      'safari-extension/**',
+      'build/**',
+      'recorder/**',
+      'coverage/**',
+      '.expo/**',
+      '.eslintrc.js',
+      '.yarnclean',
+      '.tmp/**',
+      '.vscode/**',
+      '.babel_cache/**',
+      'yarn.lock',
+      'yarn-error.log',
+      'babel.config.js',
+      'metro.config.js',
+      'jest.config.js'
+    ]
+  },
   ...ambireCommonConfig,
   {
     files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
