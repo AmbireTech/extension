e6cd578720998ae614efbc248babe153333669b8
Borislav Itskov <borislav@devlabs.bg>
2026-06-23 14:10:48 +0300

    add: verified by colibri message on transfer send form & address book

--- src/common/components/Recipient/AddToAddressBook/AddToAddressBook.tsx
+++ src/common/components/Recipient/AddToAddressBook/AddToAddressBook.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { Pressable, View } from 'react-native'
 
 import AddCircularIcon from '@common/assets/svg/AddCircularIcon'
-import AddIcon from '@common/assets/svg/AddIcon'
 import Text from '@common/components/Text'
 import { isWeb } from '@common/config/env'
 import { useTranslation } from '@common/config/localization'
 import useTheme from '@common/hooks/useTheme'
 import spacings from '@common/styles/spacings'
 import { hexToRgba } from '@common/styles/utils/common'
-import flexbox from '@common/styles/utils/flexbox'
 
 import getStyles from './styles'
 
@@ -17,6 +15,7 @@ type Props = {
   isRecipientAddressUnknown: boolean
   isRecipientHumanizerKnownTokenOrSmartContract: boolean
   addressValidationMsg: string
+  domainVerificationMessage?: string
   isRecipientAddressSameAsSender: boolean
   onAddToAddressBookPress: () => void
 }
@@ -25,45 +24,69 @@ const AddToAddressBook = ({
   isRecipientHumanizerKnownTokenOrSmartContract,
   isRecipientAddressUnknown,
   addressValidationMsg,
+  domainVerificationMessage,
   isRecipientAddressSameAsSender,
   onAddToAddressBookPress
 }: Props) => {
   const { t } = useTranslation()
   const { styles, theme } = useTheme(getStyles)
 
-  return !isRecipientHumanizerKnownTokenOrSmartContract &&
+  const shouldShowAddToAddressBook =
+    !isRecipientHumanizerKnownTokenOrSmartContract &&
     !!isRecipientAddressUnknown &&
     !isRecipientAddressSameAsSender &&
-    addressValidationMsg !== 'Invalid address.' ? (
-    <Pressable
-      style={({ hovered }: any) => [
-        ...(isWeb ? [spacings.mb] : []),
-        ...(isWeb ? [spacings.mtTy] : [spacings.mtSm]),
-        styles.addressBookButton,
-        {
-          backgroundColor: hovered
-            ? hexToRgba(theme.primaryAccent200, 0.16)
-            : theme.primaryAccent100
-        }
+    addressValidationMsg !== 'Invalid address.'
+
+  if (!domainVerificationMessage && !shouldShowAddToAddressBook) return null
+
+  return (
+    <View
+      style={[
+        ...(isWeb ? [spacings.mb, spacings.mtTy] : [spacings.mtSm]),
+        styles.confirmAddressWrapper
       ]}
-      onPress={onAddToAddressBookPress}
     >
-      <AddCircularIcon
-        width={isWeb ? 16 : 18}
-        height={isWeb ? 16 : 18}
-        style={spacings.mrMi}
-        color={theme.primaryAccent300}
-      />
-      <Text
-        fontSize={12}
-        weight="medium"
-        testID="send-form-add-to-address-book-button"
-        color={theme.primaryAccent300}
-      >
-        {t('Add to address book')}
-      </Text>
-    </Pressable>
-  ) : null
+      {!!domainVerificationMessage && (
+        <Text
+          fontSize={12}
+          weight="regular"
+          appearance="successText"
+          numberOfLines={1}
+          style={shouldShowAddToAddressBook ? spacings.mrSm : undefined}
+        >
+          {t(domainVerificationMessage)}
+        </Text>
+      )}
+      {shouldShowAddToAddressBook && (
+        <Pressable
+          style={({ hovered }: any) => [
+            styles.addressBookButton,
+            {
+              backgroundColor: hovered
+                ? hexToRgba(theme.primaryAccent200, 0.16)
+                : theme.primaryAccent100
+            }
+          ]}
+          onPress={onAddToAddressBookPress}
+        >
+          <AddCircularIcon
+            width={isWeb ? 16 : 18}
+            height={isWeb ? 16 : 18}
+            style={spacings.mrMi}
+            color={theme.primaryAccent300}
+          />
+          <Text
+            fontSize={12}
+            weight="medium"
+            testID="send-form-add-to-address-book-button"
+            color={theme.primaryAccent300}
+          >
+            {t('Add to address book')}
+          </Text>
+        </Pressable>
+      )}
+    </View>
+  )
 }
 
 export default AddToAddressBook
