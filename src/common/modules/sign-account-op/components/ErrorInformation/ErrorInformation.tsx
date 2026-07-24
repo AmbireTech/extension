080b846c5b3deb3ccdc75bb54a18c42be8784e91
Borislav Itskov <borislav@devlabs.bg>
2026-07-24 13:45:18 +0900

    add: a way for the user to refresh his pending account state upon a nonce error

--- src/common/modules/sign-account-op/components/ErrorInformation/ErrorInformation.tsx
+++ src/common/modules/sign-account-op/components/ErrorInformation/ErrorInformation.tsx
@@ -4,8 +4,10 @@ import { View } from 'react-native'
 
 import { getErrorCodeStringFromReason } from '@ambire-common/libs/errorDecoder/helpers'
 import CopyIcon from '@common/assets/svg/CopyIcon'
+import RefreshIcon from '@common/assets/svg/RefreshIcon'
 import AlertVertical from '@common/components/AlertVertical'
 import Button from '@common/components/Button'
+import ButtonWithLoader from '@common/components/ButtonWithLoader/ButtonWithLoader'
 import { useTranslation } from '@common/config/localization'
 import useController from '@common/hooks/useController'
 import useTheme from '@common/hooks/useTheme'
@@ -20,7 +22,8 @@ const ErrorInformation = () => {
   const { t } = useTranslation()
   const { styles, theme } = useTheme(getStyles)
   const { addToast } = useToast()
-  const { state: signAccountOpState } = useController('SignAccountOpController')
+  const { state: signAccountOpState, dispatch: signAccountOpDispatch } =
+    useController('SignAccountOpController')
   const { state: accountsState } = useController('AccountsController')
   const { accountStates } = accountsState
 
@@ -60,41 +63,74 @@ const ErrorInformation = () => {
     }
   }, [addToast, signAccountOpState, t, tenderlyLink])
 
-  const errorText = useMemo(() => {
-    const { code, text } = signAccountOpState?.errors?.[0] || {}
+  const refetchAccountState = useCallback(() => {
+    if (!signAccountOpState) return
 
-    if (!code && !text && !tenderlyLink) return null
+    signAccountOpDispatch({
+      type: 'method',
+      params: {
+        method: 'refetchAccountState',
+        args: []
+      }
+    })
+  }, [signAccountOpDispatch, signAccountOpState])
+
+  const errorText = useMemo(() => {
+    const { action, code, text } = signAccountOpState?.errors?.[0] || {}
+    const hasDeveloperInformation = !!code || !!text || !!tenderlyLink
+    const canRefetchAccountState = action === 'refetch-account-state'
+
+    if (!hasDeveloperInformation && !canRefetchAccountState) return null
 
     return (
       <View style={[flexbox.alignCenter]}>
-        <AlertVertical.Text
-          type="warning"
-          size="sm"
-          style={[styles.alertText, spacings.mbTy, { maxWidth: '100%' }]}
-        >
-          {code ? getErrorCodeStringFromReason(code || '', false) : text}
-        </AlertVertical.Text>
-        <Button
-          childrenPosition="left"
-          type="secondary"
-          text={t('Copy developer information')}
-          onPress={copySignAccountOpError}
-          hasBottomSpacing={false}
-          size="small"
-        >
-          <CopyIcon
-            strokeWidth={1.5}
-            width={20}
-            height={20}
-            color={theme.secondaryText}
-            style={spacings.mrTy}
+        {hasDeveloperInformation && !canRefetchAccountState && (
+          <>
+            <AlertVertical.Text
+              type="warning"
+              size="sm"
+              style={[styles.alertText, spacings.mbTy, { maxWidth: '100%' }]}
+            >
+              {code ? getErrorCodeStringFromReason(code || '', false) : text}
+            </AlertVertical.Text>
+            <Button
+              childrenPosition="left"
+              type="secondary"
+              text={t('Copy developer information')}
+              onPress={copySignAccountOpError}
+              hasBottomSpacing={false}
+              size="small"
+            >
+              <CopyIcon
+                strokeWidth={1.5}
+                width={20}
+                height={20}
+                color={theme.secondaryText}
+                style={spacings.mrTy}
+              />
+            </Button>
+          </>
+        )}
+        {canRefetchAccountState && (
+          <ButtonWithLoader
+            childrenPosition="left"
+            type="secondary"
+            text={t('Refresh state')}
+            onPress={refetchAccountState}
+            isLoading={signAccountOpState?.isRefetchingAccountState}
+            disabled={signAccountOpState?.isRefetchingAccountState}
+            size="small"
+            style={[spacings.prSm, hasDeveloperInformation ? spacings.mtTy : undefined]}
+            childrenContainerStyle={{ width: 32, ...flexbox.alignCenter }}
+            icon={<RefreshIcon width={18} height={18} />}
           />
-        </Button>
+        )}
       </View>
     )
   }, [
     copySignAccountOpError,
-    signAccountOpState?.errors,
+    refetchAccountState,
+    signAccountOpState,
     styles.alertText,
     theme.secondaryText,
     t,
