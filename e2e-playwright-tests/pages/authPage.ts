3996cd728730e99fce4bccfa4922e82f77cb81b8
sonytooo <simeonpetkov11999977@gmail.com>
2026-08-20 09:48:25 +0300

    fix: tests

--- e2e-playwright-tests/pages/authPage.ts
+++ e2e-playwright-tests/pages/authPage.ts
@@ -32,7 +32,9 @@ export class AuthPage extends BasePage {
 
   // TODO: improve method assertions
   async importViewOnlyAccount(account: string): Promise<void> {
-    await this.click(selectors.getStarted.watchAddress)
+    await this.click(selectors.getStarted.importExistingAccBtn)
+    await this.click(selectors.getStarted.showMoreBtn)
+    await this.click(selectors.getStarted.importMethodWatchAddress)
     await this.page.locator(locators.viewOnlyInputAddressField).fill(account)
     await this.click(selectors.getStarted.viewOnlyBtnImport)
     await this.setExtensionPassword()
@@ -126,7 +128,9 @@ export class AuthPage extends BasePage {
 
   // TODO: imporove method assertions
   async importCoupleOfViewOnlyAccount(account1: string, account2: string): Promise<void> {
-    await this.click(selectors.getStarted.watchAddress)
+    await this.click(selectors.getStarted.importExistingAccBtn)
+    await this.click(selectors.getStarted.showMoreBtn)
+    await this.click(selectors.getStarted.importMethodWatchAddress)
     // add address 1
     await this.page.locator(locators.viewOnlyInputAddressField).fill(account1)
     // add address 2
